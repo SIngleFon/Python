7ec6147d5cb69c043a0d68c44ca9d44d3c312d01
SIngleFon <timur5356@mail.ru>
2023-05-30 22:24:17 +0300

    added homework and groupwork from lesson 4

---
+++ groupWorkLesson4/task27.py
@@ -0,0 +1,8 @@
+# Input: She sells sea shells on the sea shore
+# # The shells that she sells are sea shells
+# # I'm sure.So if she sells sea shells on
+# # the sea shore I'm sure that the
+# # shells are sea shore shells
+
+text = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+print(len(set(input().lower().split())))
